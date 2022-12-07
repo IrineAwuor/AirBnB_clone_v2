b6586b2078656a86008b44d059b37219d936186d
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-07 09:51:24 -0800

    initial commit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,15 +6,6 @@
 --   Grants SELECT privilege for hbnb_test on performance_schema.
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL on hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost';
