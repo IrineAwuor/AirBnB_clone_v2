59f275afb76f2fe8c12b4f9f0b8a44469b128c44
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-09 01:27:41 -0800

    Reverting

--- setup_mysql_tests.sql
+++
@@ -1,8 +0,0 @@
--- create a DB
--- name hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
