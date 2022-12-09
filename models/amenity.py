39fe0fa7a5084507908c2835a0183c60b4e65a84
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-09 01:50:44 -0800

    tracking change

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity Attributes"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    # place_amenities = relationship('Place', secondary='place_amenity')
